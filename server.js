b3ac0138cb821782d2174efd1707bff86bb077ce
Tamim Al Mahi <rikabadnan@gmail.com>
2026-04-17 00:41:33 +0600

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const adminRoutes = require('./routes/adminRoutes');
-
-app.use(express.json()); // must আছে
-app.use('/api/admin', adminRoutes);
-
-const transactionRoutes = require('./routes/transactionRoutes');
-
-app.use('/api', transactionRoutes);
