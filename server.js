de98558f9991429363ab166dc83f0b409f818f9b
Tamim Al Mahi <rikabadnan@gmail.com>
2026-04-17 00:30:24 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const adminRoutes = require('./routes/adminRoutes');
 
 app.use(express.json()); // must আছে
 app.use('/api/admin', adminRoutes);
+
+const transactionRoutes = require('./routes/transactionRoutes');
+
+app.use('/api', transactionRoutes);
